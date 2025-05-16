aa2412b6da4dc6d22765e17a28f5cc48b2beb8ac
Ang Weoy Yang <weoyyang00@gmail.com>
2025-05-16 16:08:03 +0800

    feat: add twitter poc to GAME ACP

---
+++ examples/game_sdk/env.ts
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import { Address } from "viem";
+
+dotenv.config({ path: __dirname + '/.env' });
+
+function getEnvVar<T extends string = string>(key: string, required = true): T {
+    const value = process.env[key];
+    if (required && (value === undefined || value === '')) {
+        throw new Error(`${key} is not defined or is empty in the .env file`);
+    }
+    return value as T;
+}
+
+export const WHITELISTED_WALLET_PRIVATE_KEY = getEnvVar<Address>('WHITELISTED_WALLET_PRIVATE_KEY');
+export const WHITELISTED_WALLET_ENTITY_ID = parseInt(getEnvVar('WHITELISTED_WALLET_ENTITY_ID'), 10);
+export const BUYER_AGENT_WALLET_ADDRESS = getEnvVar<Address>('BUYER_AGENT_WALLET_ADDRESS');
+export const SELLER_AGENT_WALLET_ADDRESS = getEnvVar<Address>('SELLER_AGENT_WALLET_ADDRESS');
+export const BUYER_GAME_TWITTER_ACCESS_TOKEN = getEnvVar('BUYER_GAME_TWITTER_ACCESS_TOKEN');
+export const SELLER_GAME_TWITTER_ACCESS_TOKEN = getEnvVar('SELLER_GAME_TWITTER_ACCESS_TOKEN');
+export const GAME_API_KEY = getEnvVar('GAME_API_KEY');
+
+if (isNaN(WHITELISTED_WALLET_ENTITY_ID)) {
+    throw new Error('WHITELISTED_WALLET_ENTITY_ID must be a valid number in the .env file');
+}
