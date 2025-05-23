419e81a946026266c7e7d6563030af9f29c76ae4
StevenSF1998 <steven@virtuals.io>
2025-05-23 10:47:56 +0800

    fix config

--- src/configs.ts
+++ src/configs.ts
@@ -6,19 +6,22 @@ type AcpContractConfig = {
   contractAddress: Address;
   virtualsTokenAddress: Address;
   acpUrl: string;
+  alchemyRpcUrl: string;
 };
 
 const baseSepoliaAcpConfig: AcpContractConfig = {
   chain: baseSepolia,
   contractAddress: "0x2422c1c43451Eb69Ff49dfD39c4Dc8C5230fA1e6",
   virtualsTokenAddress: "0xbfAB80ccc15DF6fb7185f9498d6039317331846a",
-  acpUrl: "https://acpx.virtuals.gg",
+  alchemyRpcUrl: "https://alchemy-proxy.virtuals.io/api/proxy/rpc",
+  acpUrl: "https://acpx-staging.virtuals.io",
 };
 
 const baseAcpConfig: AcpContractConfig = {
   chain: base,
   contractAddress: "0x2422c1c43451Eb69Ff49dfD39c4Dc8C5230fA1e6",
   virtualsTokenAddress: "0xbfAB80ccc15DF6fb7185f9498d6039317331846a",
+  alchemyRpcUrl: "https://alchemy-proxy-prod.virtuals.io/api/proxy/rpc",
   acpUrl: "https://acpx.virtuals.io",
 };
 
