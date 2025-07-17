a7e12b5b7a5289c3b94ab3fc37501c5de74a96f8
Ang Weoy Yang <weoyyang00@gmail.com>
2025-07-17 10:35:13 +0800

    feat: add examples to funds transfer

---
+++ examples/acp-base/funds/buyer.ts
@@ -0,0 +1,152 @@
+// TODO: Update import to @virtuals-protocol/acp-node after package update
+
+import AcpClient, {
+  AcpContractClient,
+  AcpJob,
+  AcpJobPhases,
+  AcpMemo,
+  baseSepoliaAcpConfig,
+  MemoType,
+} from "../../../src";
+import {BUYER_AGENT_WALLET_ADDRESS, BUYER_ENTITY_ID, WHITELISTED_WALLET_PRIVATE_KEY,} from "./env";
+import {PayloadType} from "../../../src/interfaces";
+
+function delay(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function buyer() {
+
+  let job: AcpJob | undefined = undefined;
+
+  const acpClient = new AcpClient({
+    acpContractClient: await AcpContractClient.build(
+      WHITELISTED_WALLET_PRIVATE_KEY,
+      BUYER_ENTITY_ID,
+      BUYER_AGENT_WALLET_ADDRESS,
+      baseSepoliaAcpConfig
+    ),
+    onNewTask: async (job: AcpJob, memoToSign?: AcpMemo) => {
+      if (
+        job.phase === AcpJobPhases.NEGOTIATION &&
+        memoToSign?.nextPhase === AcpJobPhases.TRANSACTION
+      ) {
+        console.log("Paying job", job, "with memo", memoToSign.id);
+        await job.pay(job.price);
+        console.log(`Job ${job.id} paid`);
+
+        // Buyer starts opening positions
+        console.log("Opening position", job);
+        await job.openPosition(
+            [
+              {
+                symbol: "BTC",
+                amount: 1, // amount in $VIRTUAL
+                tp: { percentage: 5 },
+                sl: { percentage: 2 },
+              },
+              {
+                symbol: "ETH",
+                amount: 2, // amount in $VIRTUAL
+                tp: { percentage: 10 },
+                sl: { percentage: 5 },
+              }
+            ],
+            0.005 // fee amount in $VIRTUAL
+        );
+        console.log(`Job ${job.id} 2 positions opened`);
+
+        // Buyer open 1 more position
+        await delay(10000);
+        await job.openPosition(
+            [
+              {
+                symbol: "VIRTUAL",
+                amount: 3, // amount in $VIRTUAL
+                tp: { percentage: 33000 },
+                sl: { percentage: 2 },
+              }
+            ],
+            0.005 // fee amount in $VIRTUAL
+        );
+        console.log(`Job ${job.id} 1 more position opened`);
+
+        // Buyer starts closing positions on initiative, before TP/SL hit
+        await delay(100000);
+        await job.closePosition({
+          symbol: "BTC",
+          amount: 1.01
+        })
+        console.log(`Job ${job.id} BTC position closed`);
+
+        // Buyer close job upon all positions return
+        await delay(50000);
+        await job.closeJob();
+        console.log(`Job ${job.id} closed`);
+        return;
+      }
+
+      // receiving funds transfer from provider for the unfulfilled positions
+      if (
+          job.phase === AcpJobPhases.TRANSACTION &&
+          memoToSign?.nextPhase === AcpJobPhases.TRANSACTION &&
+          memoToSign?.type === MemoType.PAYABLE_TRANSFER
+      ) {
+        console.log("Accepting funds transfer", job, "with memo", memoToSign.id);
+        if (memoToSign?.payloadType === PayloadType.UNFULFILLED_POSITION) {
+          await job.responseUnfulfilledPosition(memoToSign?.id, true, "Accepting funds transfer for the unfulfilled positions");
+          console.log(`Job ${job.id} funds transfer for the unfulfilled position accepted`);
+          return
+        }
+
+        await job.responsePositionFulfilled(memoToSign?.id, true, "Accepting funds transfer for the fulfilled positions");
+        console.log(`Job ${job.id} funds transfer for the fulfilled position accepted`);
+        return;
+      }
+
+      // receiving funds transfer from provider at closing of the job
+      if (
+        job.phase === AcpJobPhases.TRANSACTION &&
+        memoToSign?.type === MemoType.PAYABLE_TRANSFER &&
+        memoToSign?.nextPhase === AcpJobPhases.EVALUATION // if phase is evaluation, it means the job is closing
+      ) {
+        console.log("Accepting funds transfer", job, "with memo", memoToSign.id);
+        await job.confirmJobClosure(memoToSign?.id, true);
+        console.log(`Job ${job.id} funds transfer accepted`);
+        return;
+      }
+
+      if (job.phase === AcpJobPhases.COMPLETED) {
+        console.log(`Job ${job.id} completed`);
+        return;
+      }
+    },
+    onEvaluate: async (job: AcpJob) => {
+      console.log("Evaluation function called", job);
+      await job.evaluate(true, "Self-evaluated and approved");
+      console.log(`Job ${job.id} evaluated`);
+    },
+  });
+
+  // Browse available agents based on a keyword and cluster name
+  const relevantAgents = await acpClient.browseAgents(
+      "Funds TT (Seller)",
+      {
+        graduated: false
+      }
+  );
+  // Pick one of the agents based on your criteria (in this example we just pick the first one)
+  const chosenAgent = relevantAgents[0];
+  // Pick one of the service offerings based on your criteria (in this example we just pick the first one)
+  const chosenJobOffering = chosenAgent.offerings[0];
+
+  const jobId = await chosenJobOffering.initiateJob(
+      "I have 8 $VIRTUAL, ape it for me.",
+      BUYER_AGENT_WALLET_ADDRESS,// Use default evaluator address
+      new Date(Date.now() + 1000 * 60 * 6) // expiredAt as last parameter
+  );
+
+  console.log(`Job ${jobId} initiated`);
+}
+
+buyer();
---
+++ examples/acp-base/funds/seller.ts
@@ -0,0 +1,136 @@
+// TODO: Update import to @virtuals-protocol/acp-node after package update
+
+import AcpClient, {
+  AcpContractClient,
+  AcpJob,
+  AcpJobPhases,
+  AcpMemo,
+  baseSepoliaAcpConfig,
+  MemoType,
+} from "../../../src";
+import { SELLER_AGENT_WALLET_ADDRESS, SELLER_ENTITY_ID, WHITELISTED_WALLET_PRIVATE_KEY } from "./env";
+import { FundResponsePayload, PayloadType } from "../../../src/interfaces";
+
+function delay(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function seller() {
+  // for simulation only
+  let positionFulFilledCount = 0;
+
+  new AcpClient({
+    acpContractClient: await AcpContractClient.build(
+      WHITELISTED_WALLET_PRIVATE_KEY,
+      SELLER_ENTITY_ID,
+      SELLER_AGENT_WALLET_ADDRESS,
+      baseSepoliaAcpConfig
+    ),
+    onNewTask: async (job: AcpJob, memoToSign?: AcpMemo) => {
+      if (
+        job.phase === AcpJobPhases.REQUEST &&
+        memoToSign?.nextPhase === AcpJobPhases.NEGOTIATION
+      ) {
+        console.log("Responding to job", job, "with memo", memoToSign.id);
+        await job.respond<FundResponsePayload>(
+            true,
+            {
+              type: PayloadType.FUND_RESPONSE,
+              data: {
+                reportingApiEndpoint: "https://example-reporting-api-endpoint/positions"
+              }
+            }
+        );
+        console.log(`Job ${job.id} responded`);
+        return;
+      }
+
+      if (
+        job.phase === AcpJobPhases.TRANSACTION &&
+        memoToSign?.nextPhase === AcpJobPhases.TRANSACTION &&
+        memoToSign.type !== MemoType.MESSAGE
+      ) {
+        // opening positions for client
+        if (memoToSign?.type === MemoType.PAYABLE_TRANSFER) {
+          console.log("Accepting positions opening", job, "with memo", memoToSign.id);
+          await job.responseOpenPosition(memoToSign?.id, true, "accepts position opening");
+          console.log(memoToSign)
+          console.log(`Job ${job.id} position opening accepted`);
+
+          if (positionFulFilledCount === 0) {
+              positionFulFilledCount += 1;
+              // Seller starts closing positions on TP/SL hit (Delay for simulation, real world scenario should be triggered when real tp/sl hit)
+              await delay(50000);
+              await job.positionFulfilled(
+                  {
+                      symbol: "VIRTUAL",
+                      amount: 99,
+                      contractAddress: "0x0b3e328455c4059EEb9e3f84b5543F74E24e7E1b",
+                      type: "TP",
+                      pnl: 96,
+                      entryPrice: 1.8,
+                      exitPrice: 59.4
+                  }
+              )
+              console.log(`Job ${job.id} VIRTUAL TP fulfilled`);
+
+              // Transfer unfulfilled amount back to buyer
+              await delay(40000);
+              await job.unfulfilledPosition(
+                  {
+                      symbol: "ETH",
+                      amount: 1.5,
+                      contractAddress: "0xd449119E89773693D573ED217981659028C7662E",
+                      type: "PARTIAL"
+                  }
+              )
+              console.log(`Job ${job.id} ETH position partially fulfilled, returning the remainders`);
+          }
+          return;
+        }
+
+        // closing positions for client
+        if (memoToSign?.type === MemoType.PAYABLE_REQUEST) {
+          console.log("Accepting positions closing", job, "with memo", memoToSign.id);
+          await job.responseClosePosition(memoToSign?.id, true, "accepts position closing");
+          console.log(memoToSign)
+          console.log(`Job ${job.id} position closing accepted`);
+
+          return;
+        }
+        return;
+      }
+
+      // closing the job
+      if (
+        job.phase === AcpJobPhases.TRANSACTION &&
+        memoToSign?.type === MemoType.MESSAGE
+      ) {
+        if (job.memos.length > 3) {
+          console.log("Closing job", job, "with memo", memoToSign.id);
+          // close job with remaining positions (not TP/SL nor closed by buyer)
+          await job.responseCloseJob(
+              memoToSign?.id,
+              true,
+              [
+                  {
+                      symbol: "ETH",
+                      amount: 0.5,
+                      contractAddress: "0xd449119E89773693D573ED217981659028C7662E",
+                      type: "CLOSE",
+                      pnl: 0,
+                      entryPrice: 3000,
+                      exitPrice: 3000
+                  }
+              ]
+          ); // transfer amount to client (closing the job)
+          console.log(`Job ${job.id} closed`);
+
+          return;
+        }
+      }
+    },
+  });
+}
+
+seller();
